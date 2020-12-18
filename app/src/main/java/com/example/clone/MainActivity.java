ada5d4c76b66519c1379ef50f845e9ef0db36503
ajsof <ajsofttech19@gmail.com>
2020-12-19 00:28:56 +0530

    TestingCommit

--- app/src/main/java/com/example/clone/MainActivity.java
+++ app/src/main/java/com/example/clone/MainActivity.java
@@ -12,7 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+//Tested
     Button login,register;
   FirebaseUser firebaseUser;
 
